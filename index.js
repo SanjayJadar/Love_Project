0c34577b0b588886b4c1548a9f9f24dd38045078
Sanjay <sanjayjadar53@gmail.com>
2023-10-04 15:10:42 +0530

    port change

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ app.post('/', async(req, res)=>{
     }
 })
 
-app.listen(5000,()=>{
-    console.log('Server is running');
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>{
+    console.log('Server is running'+port);
 })
