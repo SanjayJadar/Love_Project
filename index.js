17388d76899f94e3f4ce87dea71ee55ca0ed46ad
Sanjay <sanjayjadar53@gmail.com>
2023-10-04 15:03:35 +0530

    mongodb change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}))
 
-mongoose.connect('mongodb://127.0.0.1:27017/LoversDB')
+mongoose.connect('mongodb+srv://sanjayjadar53:VD2Ardjzr4tIekON@portfolio.76hvi7e.mongodb.net/LoversDB?retryWrites=true&w=majority')
 .then(()=>{console.log('Database Connected')})
 .catch((e)=>{console.log("Error message : ", e.message)})
 
@@ -20,9 +20,10 @@ app.post('/', async(req, res)=>{
     const data = new User(req.body);
     await data.save()
     .then(()=>{console.log('Sent')})
-    .catch((e)=>{console.log('Not Sent',e.message)})
-    console.log(data);
-    res.render('end');
+    .catch((e)=>{console.log('Not Sent',e.message)}) 
+    if(data){
+        res.render('end');
+    }
 })
 
 app.listen(5000,()=>{
